a626480735279f7326cc6255924944b54ccf245d
JoshGuarino <guar0638@fredonia.edu>
2019-01-13 20:52:35 -0500

    added 404 and 500 error checking

--- app.py
+++ app.py
@@ -19,6 +19,15 @@ def skills():
 def contact():
     return render_template('contact.html', title="Contact")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', the_error="404 Not Found", message="Sorry! The page you requested doesn't exist!"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error.html', the_error="500 Internal Server Error", message="Sorry! My website is experiencing diffculties..."), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
