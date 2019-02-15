494a3613818f0ae928f3dede044f630fd103b61f
JoshGuarino <guar0638@fredonia.edu>
2019-02-15 13:51:42 -0500

    did this not work??

--- static/js/skills.js
+++ static/js/skills.js
@@ -1,11 +1,15 @@
-const data = [{section: "Programming Languages", info: "C/C++, Python, Javascript(node.js), PHP, HTML, CSS"}, 
-              {section: "Frameworks & Libraries", info: "Bootstrap, JQuery, react.js, vue.js, express.js, flask, django"}, 
-              {section: "Package Managers", info: "npm, pip"}, 
-              {section: "Database Software", info: "MS SQL Server, MySql, MongoDB, PostgreSQL"}, 
-              {section: "Version Control w/ GIT", info: "gitlab, github"}, 
-              {section: "Software Testing/QA", info: "automation(selenium, sikuli), regression, performance"}];
+const data = [{section: "Programming Languages", content: ["C/C++", "Python", "Javascript(node.js)", "PHP", "HTML", "CSS"], info: ""}, 
+              {section: "Frameworks & Libraries", content: ["Bootstrap", "JQuery", "react.js", "vue.js", "express.js", "flask", "django", "laravel"], info: ""}, 
+              {section: "Package Managers", content: ["npm", "pip", "composer"], info: ""}, 
+              {section: "Database Software", content:["MS SQL Server", "MySql", "MongoDB", "PostgreSQL"], info: ""}, 
+              {section: "Version Control w/ GIT", content: ["gitlab", "github"], info: ""}, 
+              {section: "Software Testing/QA", content: ["selenium", "sikuli", "regression", "performance"], info: ""}];
 
 const populateModal = (arr_pos) =>{
+    $('#m_content').html('');
+    $('#m_title').html('');
     $('#m_title').html(data[arr_pos].section);
-    $('#m_content').html(data[arr_pos].info);
-}
+    data[arr_pos].content.forEach(element => {
+        $('#m_content').append('<li>' + element + '</li>');
+    });
+};
