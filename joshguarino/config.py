44099d2a94a5951a7c5e49d6f40566bcb6073458
JoshGuarino <guar0638@fredonia.edu>
2020-06-16 14:49:16 -0400

    switched to yaml config

--- joshguarino/config.py
+++ joshguarino/config.py
@@ -1,4 +1,5 @@
 from key_gen import generate_key
+import yaml
 import json
 
 class Config(object):
@@ -15,11 +16,11 @@ class Config(object):
     RECAPTCHA_PRIVATE_KEY = ''
     RECAPTCHA_OPTIONS = {'theme':'black'}
     def get_config():
-        json_data = open('config.json').read()
-        conf = json.loads(json_data)
-        Config.MAIL_USERNAME = conf['email']
-        Config.MAIL_PASSWORD = conf['password']
-        Config.MAIL_RECIEVE = conf['r_email']
-        Config.MAIL_SERVER = conf['serv']
-        Config.RECAPTCHA_PUBLIC_KEY = conf['RECAPTCHA_PUBLIC_KEY']
-        Config.RECAPTCHA_PRIVATE_KEY = conf['RECAPTCHA_PRIVATE_KEY']
+        with open('config.yaml') as f:
+            data = yaml.safe_load(f)
+        Config.MAIL_USERNAME = data['email']
+        Config.MAIL_PASSWORD = data['password']
+        Config.MAIL_RECIEVE = data['r_email']
+        Config.MAIL_SERVER = data['serv']
+        Config.RECAPTCHA_PUBLIC_KEY = data['RECAPTCHA_PUBLIC_KEY']
+        Config.RECAPTCHA_PRIVATE_KEY = data['RECAPTCHA_PRIVATE_KEY']
