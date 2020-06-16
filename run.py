dba7845b68039e49a4563b87d6381fd0a201ca4c
JoshGuarino <guar0638@fredonia.edu>
2020-06-16 14:16:05 -0400

    added platform specfic run check

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
 import subprocess
+import platform
 
 def run_app():
     os.chdir('joshguarino')
-    subprocess.call(["py", "app.py"])
+    if platform.system() == 'Windows':
+        subprocess.call(["py", "app.py"])
+        return
+    subprocess.call(["python3", "app.py"])
 
 def dep_install():
     subprocess.call(["pipenv", "install"])   
