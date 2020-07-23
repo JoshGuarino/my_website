c5e742ada6393bed3f5c6a439dba79288a858b40
JoshGuarino <guar0638@fredonia.edu>
2020-07-23 19:33:26 -0400

    Fixed/tweaked coor button + css cleanup

--- joshguarino/static/js/color_mode.js
+++ joshguarino/static/js/color_mode.js
@@ -1,10 +1,12 @@
 function toggle_color(){
-    if (getComputedStyle(document.documentElement).getPropertyValue('--dark')==='whitesmoke'){
-        document.documentElement.style.setProperty('--dark', 'black');
-        document.documentElement.style.setProperty('--light', 'whitesmoke');
+    if (getComputedStyle(document.documentElement).getPropertyValue('--dark')==='black'){
+        document.documentElement.style.setProperty('--dark', 'whitesmoke');
+        document.documentElement.style.setProperty('--light', 'black');
+        document.getElementById('color_mode').innerHTML = 'Light';
         return;
     }
-    document.documentElement.style.setProperty('--dark', 'whitesmoke');
-    document.documentElement.style.setProperty('--light', 'black');
+    document.documentElement.style.setProperty('--dark', 'black');
+    document.documentElement.style.setProperty('--light', 'whitesmoke');
+    document.getElementById('color_mode').innerHTML = 'Dark';
     return;
 }
