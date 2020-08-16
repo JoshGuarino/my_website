5622f8260d40f31c6b0d3cb3e2821c120da00f7c
JoshGuarino <guar0638@fredonia.edu>
2020-08-16 01:44:52 -0400

    code cleanup

--- src/graphql.py
+++ src/graphql.py
@@ -7,9 +7,9 @@ class GraphQL:
         self.query = None
 
     def get(self, query):
-        self.query = { 'query' : query }
+        self.query = { 'query' : query }\
+        response = requests.get(url=self.url, json=self.query, headers=self.headers)
         if response.status_code == 200:
-            response = requests.get(url=self.url, json=self.query, headers=self.headers)
             return response.json
         return f'Got unexpected status code {response.status_code}: {response.content}'
 
@@ -17,6 +17,5 @@ class GraphQL:
         self.query = { 'query' : query }
         response = requests.post(url=self.url, json=self.query, headers=self.headers)
         if response.status_code == 200:
-            
             return response.json()
         return f'Got unexpected status code {response.status_code}: {response.content}'
