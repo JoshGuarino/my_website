3dc317c54ff8c1127ea3f1ed78487596d05c4df5
JoshGuarino <guar0638@fredonia.edu>
2020-08-22 15:03:59 -0400

    copyright will update dynamically now

--- src/static/js/datetime.js
+++ src/static/js/datetime.js
@@ -26,47 +26,36 @@ const getDateTime = (param) => {
         case 0:
         month = 'January';
         break;
-
         case 1:
         month = 'February';
         break;
-
         case 2:
         month = 'March';
         break;
-
         case 3:
         month = 'April';
         break;
-
         case 4:
         month = 'May';
         break;
-
         case 5:
         month = 'June';
         break;
-
         case 6:
         month = 'July';
         break;
-
         case 7:
         month = 'August';
         break;
-
         case 8:
         month = 'September';
         break;
-
         case 9:
         month = 'October';
         break;
-
         case 10:
         month = 'November';
         break;
-
         case 11:
         month = 'December';
         break;
@@ -76,39 +65,38 @@ const getDateTime = (param) => {
         case 0:
         weekday = 'Sunday';
         break;
-
         case 1:
         weekday = 'Monday';
         break;
-
         case 2:
         weekday = 'Tuesday';
         break;
-
         case 3:
         weekday = 'Wednesday';
         break;
-
         case 4:
         weekday = 'Thursday';
         break;
-
         case 5:
         weekday = 'Friday';
         break;
-
         case 6:
         weekday = 'Saturday';
         break;
     }
 
+    let copyright_elems = document.querySelectorAll('.copyright'); 
+    copyright_elems.forEach(element => {
+        element.innerHTML = `© 2019-${date.getFullYear()} Josh Guarino`;
+    });
+
     if(param ==="home"){
         document.getElementById('clock').innerHTML = `<h2 id="time">${hour}:${min}:${sec} ${session}<br>${weekday}, ${month} ${day}, ${year}</h2>`;
         document.getElementById('nav_clock').innerHTML = `${hour}:${min}:${sec} ${session}`;
+        return;
     }
-    else if(param === "not_home"){
-        document.getElementById('nav_clock').innerHTML = `${hour}:${min}:${sec} ${session}`;
-    }
+    document.getElementById('nav_clock').innerHTML = `${hour}:${min}:${sec} ${session}`;
+    return;
 };
 
 const startClock = (param) => {
