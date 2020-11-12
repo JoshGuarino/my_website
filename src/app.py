96870932af4ab527aca79e7135678a2680c5e07f
JoshGuarino <guar0638@fredonia.edu>
2020-11-12 17:26:14 -0500

    fixed error

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ def webhook():
     origin = repo.remotes.origin
     origin.pull()
     pa = PythonAnywhere(app.config['PA_TOKEN'])
-    pa.post(config['pa_reload'])
+    pa.post(app.config['pa_reload'])
     return 'Updated app successfully.', 200
 
 @app.route("/")
